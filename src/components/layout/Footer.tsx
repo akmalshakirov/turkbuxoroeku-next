5d71e5743f0190e174bb1ab89285650fd784b89a
akmal <akmalpubg221@gmail.com>
2025-06-01 17:21:42 +0500

    FOOTER IS DONE

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,38 +1,29 @@
-import { Instagram, Facebook, Youtube, Send } from "lucide-react";
-import Logo from "../../../public/icon.svg";
+import { Facebook, Instagram, Send, Youtube } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useRef } from "react";
 
 const Footer = () => {
-    useEffect(() => {
-        const links = document.querySelectorAll<HTMLAnchorElement>(".group");
-        links.forEach((link) => {
-            const handleMouseMove = (e: MouseEvent) => {
-                const rect = link.getBoundingClientRect();
-                const x = e.clientX - rect.left;
-                const y = e.clientY - rect.top;
-                link.style.setProperty("--tw-hover-origin", `${x}px ${y}px`);
-            };
+    const linkRef = useRef<HTMLAnchorElement>(null);
 
-            link.addEventListener("mousemove", handleMouseMove);
-
-            return () => {
-                link.removeEventListener("mousemove", handleMouseMove);
-            };
-        });
-    }, []);
+    const handleMouseMove = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        if (!linkRef.current) return;
+        const rect = linkRef.current.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        linkRef.current.style.setProperty("--tw-hover-origin", `${x}px ${y}px`);
+    };
 
     return (
-        <footer className='bg-pink-600 text-white py-8'>
+        <footer className='bg-[#cb4081] text-white py-8'>
             <div className='flex gap-[4.392vw] justify-center'>
                 <div className='mb-6 md:mb-0'>
                     <Image
-                        src={Logo}
+                        src='/icon.svg'
                         alt='Logo'
                         className='mb-4'
-                        width={150}
-                        height={150}
+                        width={190}
+                        height={190}
                     />
                     <h4 className='text-lg font-bold mb-2'>
                         ТУРК БУХАРА ЭКУ ЦЕНТР
@@ -41,18 +32,26 @@ const Footer = () => {
                     <p>© 2015 - 2025</p>
                     <p className='mt-4'>Мы в социальных сетях:</p>
                     <div className='flex space-x-4 mt-2'>
-                        <a href='/' className='text-white text-xl group'>
-                            <Instagram className='group-hover:text-[#e9acb4] transition' />
-                        </a>
-                        <a href='/' className='text-white text-xl group'>
-                            <Send className='group-hover:text-[#e9acb4] transition' />
-                        </a>
-                        <a href='/' className='text-white text-xl group'>
-                            <Facebook className='group-hover:text-[#e9acb4] transition' />
-                        </a>
-                        <a href='/' className='text-white text-xl group'>
-                            <Youtube className='group-hover:text-[#e9acb4] transition' />
-                        </a>
+                        <Link
+                            href='/'
+                            className='hover:bg-[var(--primary-color)] border rounded-full bg-pink-500 text-xl p-2'>
+                            <Instagram size={20} />
+                        </Link>
+                        <Link
+                            href='/'
+                            className='hover:bg-[var(--primary-color)] transition border rounded-full bg-pink-500 text-xl p-2'>
+                            <Send size={20} />
+                        </Link>
+                        <Link
+                            href='/'
+                            className='hover:bg-[var(--primary-color)] transition border rounded-full bg-pink-500 text-xl p-2'>
+                            <Facebook size={20} />
+                        </Link>
+                        <Link
+                            href='/'
+                            className='hover:bg-[var(--primary-color)] transition border rounded-full bg-pink-500 text-xl p-2'>
+                            <Youtube size={20} />
+                        </Link>
                     </div>
                 </div>
                 <div className='mb-6 md:mb-0'>
@@ -67,31 +66,49 @@ const Footer = () => {
                     <p>+998 99 118 03-03</p>
                     <p>Ахмад Юнаки 43А ТТЗ</p>
                 </div>
-                <div className=''>
-                    <h4 className='text-lg font-bold mb-2'>О нас</h4>
+                <div>
+                    <h4 className='text-lg font-bold mb-2'>
+                        <Link href='/about'>О нас</Link>
+                    </h4>
                     <ul className='space-y-2'>
-                        <li>Процедуры и процессы</li>
-                        <li>Ресурсы и поддержка</li>
-                        <li>Контакты</li>
-                        <li>Услуги</li>
-                        <li>Успешные истории</li>
-                        <li>Новости и блог</li>
+                        <li>
+                            <Link href='/'>Процедуры и процессы</Link>
+                        </li>
+                        <li>
+                            <Link href='/'>Ресурсы и поддержка</Link>
+                        </li>
+                        <li>
+                            <Link href='/contacts'>Контакты</Link>
+                        </li>
+                        <li>
+                            <Link href='/services'>Услуги</Link>
+                        </li>
+                        <li>
+                            <Link href='/'>Успешные истории</Link>
+                        </li>
+                        <li>
+                            <Link href='/news'>Новости и блог</Link>
+                        </li>
                     </ul>
                 </div>
             </div>
             <div className='text-center mt-8 flex items-center justify-center'>
-                Developed by:{" "}
+                <p className='mr-1'>Все права защищены ©. </p>
+                <p>Разработано:</p>
                 <Link
                     href='https://limon.group'
+                    target='_blank'
                     rel='noopener noreferrer'
                     title='Limon Group'
-                    target='_blank'
+                    ref={linkRef}
+                    onMouseMove={handleMouseMove}
                     className='relative inline-block px-4 py-2 font-semibold text-white transition-all duration-500 rounded-lg group overflow-hidden no-underline!'>
                     <span
-                        className='absolute inset-0 w-full h-full bg-gradient-to-r from-pink-500 via-yellow-500 to-orange-500 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-500'
+                        className='absolute inset-0 w-full h-full bg-gradient-to-r from-pink-500 via-yellow-500 to-orange-500 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300'
                         style={{
                             transformOrigin: "var(--tw-hover-origin)",
                         }}></span>
+
                     <span className='relative z-10 transition-colors duration-300'>
                         Limon.group
                     </span>
